c028a4f7469cd25330d8ebd599a906e0d675e075
fethullahcucu <fethullahcucu@gmail.com>
2025-08-05 13:01:00 +0100

    serializers first version. curl http://127.0.0.1:8000/api/books/ | jq gives you first object (dune) as json format

--- bookcatalog/api/models.py
+++ bookcatalog/api/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    author = models. CharField(max_length=255)
+    author = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
