f9c6a3247ee653c865dd52d7479b9ad6049518cb
fethullahcucu <fethullahcucu@gmail.com>
2025-08-05 13:36:43 +0100

    second serialization. we add post serialization with curl -H *quote Content-Type:application *quote -X POST http://127.0.0.1:8000/api/books/ 0d '*quote title *quote : *quote title name *quote , *quote author : *quote author name . also we create demo db object to pass test

--- bookcatalog/api/tests/test_views.py
+++ bookcatalog/api/tests/test_views.py
@@ -4,11 +4,16 @@ from django.urls import reverse
 
 class BookViewTest(APITestCase):
     def test_response_is_correct(self):
+        book = Book.objects.create(title="Demo",description="Demo book", author="Demo Author Name")
+
+
         url = reverse('api:books')
         response = self.client.get(url, format='json')
         assert response.status_code == status.HTTP_200_OK
         body = response.json()
-        assert body['hello'] == 'django'
+        assert body[0]['title'] == book.title
+        assert body[0]['description'] == book.description
+        assert body[0]['author'] == book.author
 
 
 class HealthViewTest(APITestCase):
--- bookcatalog/api/views.py
+++ bookcatalog/api/views.py
@@ -20,7 +20,14 @@ class BookView(APIView):
         serializer = BookSerializer(all_books, many=True)
         return Response(serializer.data)
 
-    
+    def post (self, request, *args, **kwargs):
+        data = request.data
+
+        serializer = BookSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+        
 
 
 book_view = BookView.as_view()
